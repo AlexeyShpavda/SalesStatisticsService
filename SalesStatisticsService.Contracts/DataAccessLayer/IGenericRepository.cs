12eb388466cd68596093301c2ba36a7e4801a58b
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-12-12 23:53:08 +0300

    Add Generic Repository Interface to Contracts Assembly

---
+++ SalesStatisticsService.Contracts/DataAccessLayer/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace SalesStatisticsService.Contracts.DataAccessLayer
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        void Add(TEntity entity);
+
+        void Remove(TEntity entity);
+
+        IQueryable<TEntity> GetAll();
+
+        IQueryable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+    }
+}
