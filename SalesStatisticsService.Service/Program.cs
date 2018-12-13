2598b0b7b0f0b4ebe7260824ba11baaf58b44792
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-12-14 02:14:10 +0300

    Add Assembly for Windows Service

---
+++ SalesStatisticsService.Service/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.ServiceProcess;
+
+namespace SalesStatisticsService.Service
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        private static void Main()
+        {
+            var servicesToRun = new ServiceBase[]
+            {
+                new Service1()
+            };
+            ServiceBase.Run(servicesToRun);
+        }
+    }
+}
