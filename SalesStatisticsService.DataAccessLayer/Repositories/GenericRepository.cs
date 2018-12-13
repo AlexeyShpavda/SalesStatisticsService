9923824204a4c9a074f13bad0f840a9d739ff829
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-12-13 11:58:20 +0300

    Write Implementation of IGenericRepository

---
+++ SalesStatisticsService.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using SalesStatisticsService.Contracts.DataAccessLayer;
+
+namespace SalesStatisticsService.DataAccessLayer.Repositories
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class 
+    {
+        private readonly DbSet<TEntity> _dbSet;
+        private readonly DbContext _dbContext;
+
+        public GenericRepository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = dbContext.Set<TEntity>();
+        }
+
+        public void Add(TEntity entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            if (_dbContext.Entry(entity).State == EntityState.Detached)
+            {
+                _dbSet.Attach(entity);
+            }
+
+            _dbSet.Remove(entity);
+        }
+
+        public IQueryable<TEntity> GetAll()
+        {
+            return _dbSet;
+        }
+
+        public IQueryable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            return _dbSet.Where(predicate);
+        }
+    }
+}
