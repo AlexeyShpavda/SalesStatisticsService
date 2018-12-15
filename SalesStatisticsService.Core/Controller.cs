8a1cfb1fcb6f24e0383d33f76a5c2e81674b4e94
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-12-15 17:51:29 +0300

    Add Method to Convert Strings to Data Transfer Objects

--- SalesStatisticsService.Core/Controller.cs
+++ SalesStatisticsService.Core/Controller.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using SalesStatisticsService.Contracts.Core;
 using SalesStatisticsService.Contracts.Core.DataTransferObjects;
@@ -39,14 +42,72 @@ namespace SalesStatisticsService.Core
 
         public void ProcessFile(object source, FileSystemEventArgs e)
         {
-            var task = Task<IEnumerable<SaleDto>>.Factory.StartNew(() => _parser.ParseFile(e.FullPath));
+            //var task = Task<IEnumerable<IFileContent>>.Factory.StartNew(() =>
+            //{
+            //    WriteToDatabase(_parser.ParseFile(e.FullPath));
+            //});
+            var fileNameSplitter = '_';
+            Task.Run(() =>
+                {
+                    WriteToDatabase(
+                        _parser.ParseFile(e.FullPath),
+                        _parser.ParseLine(e.Name, fileNameSplitter).First());
+                });
+            //task.ContinueWith((t, o) => WriteToDatabase(t.Result), null,
+            //   TaskScheduler.FromCurrentSynchronizationContext());
 
-            task.ContinueWith((t, o) => WriteToDatabase(t.Result), null,
-                TaskScheduler.FromCurrentSynchronizationContext());
+            //task.ContinueWith((t, o) => WriteToDatabase(t.Result), null,
+            //   TaskScheduler.FromCurrentSynchronizationContext());
         }
 
-        private void WriteToDatabase(IEnumerable<SaleDto> sales)
+        private void WriteToDatabase(IEnumerable<IFileContent> sales, string managerLastName)
         {
+            IList<SaleDto> saleDtos = new List<SaleDto>();
+            foreach (var sale in sales)
+            {
+                //_saleUnitOfWork.Add(saleDtos.Add(CreateDataTransferObjects<SaleDto>(sale, managerLastName)));
+                _saleUnitOfWork.Add(CreateDataTransferObjects<SaleDto>(sale, managerLastName));
+            }
+        }
+
+        private SaleDto CreateDataTransferObjects<T> (IFileContent sale, string managerName)
+        {
+            IList<string> name = _parser.ParseLine(sale.Customer, ' ');
+            CustomerDto customerDto = new CustomerDto();
+            customerDto.LastName = name[0];
+            customerDto.FirstName = name[1];
+
+
+
+            ManagerDto managerDto = new ManagerDto();
+            managerDto.LastName = managerName;
+
+            ProductDto product = new ProductDto();
+            product.Name = sale.Product;
+
+
+            string timeString = sale.Date.Replace('.', '/');
+            DateTime dateTime = DateTime.ParseExact(timeString, "dd/MM/yyyy", null);
+
+            //DateTime dateTime = System.DateTime.Parse(timeString);
+            //dateTime.ToString("dd/mm/yyyy");
+
+            //dateTime.Date = timeString;
+            //DateTime dateTime = DateTime.Parse(timeString);
+
+            decimal sum = decimal.Parse(sale.Sum);
+
+            SaleDto saleDto = new SaleDto();
+
+            saleDto.Manager = managerDto;
+            saleDto.Product = product;
+            saleDto.Customer = customerDto;
+            saleDto.Date = dateTime;
+            saleDto.Sum = sum;
+
+
+            return saleDto;
+            //managerDto.LastName =
         }
     }
 }
