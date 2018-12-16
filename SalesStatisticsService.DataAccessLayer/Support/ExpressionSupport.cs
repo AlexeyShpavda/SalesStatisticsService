954359431568e139d13990cda7b170efa2506650
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-12-16 07:53:52 +0300

    Add Method to Convert Expression Tree

---
+++ SalesStatisticsService.DataAccessLayer/Support/ExpressionSupport.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace SalesStatisticsService.DataAccessLayer.Support
+{
+    public static class ExpressionSupport
+    {
+        public static Expression<Func<TTargetType, bool>> Project<TSourceType, TTargetType>(this Expression<Func<TSourceType, bool>> sourceExpression)
+        {
+            ParameterExpression sourceParameter = sourceExpression.Parameters.FirstOrDefault();
+            ParameterExpression targetParameter = Expression.Parameter(typeof(TTargetType), sourceParameter.Name);
+            Expression newBody = new TransformVisitor<TTargetType>(sourceParameter, targetParameter).Visit(sourceExpression.Body);
+            var paramsList = sourceExpression.Parameters.ToList();
+            var position = paramsList.IndexOf(sourceParameter);
+            paramsList[position] = targetParameter;
+            return Expression.Lambda<Func<TTargetType, bool>>(newBody, paramsList);
+        }
+
+
+        private class TransformVisitor<TTarget> : ExpressionVisitor
+        {
+            private readonly ParameterExpression _targetParameter;
+            private readonly ParameterExpression _sourceParameter;
+
+            public TransformVisitor(ParameterExpression sourceParameter, ParameterExpression targetParameter)
+            {
+                _sourceParameter = sourceParameter;
+                _targetParameter = targetParameter;
+            }
+
+            protected override Expression VisitParameter(ParameterExpression node)
+            {
+                return node == _sourceParameter ? _targetParameter : base.VisitParameter(node);
+            }
+
+            protected override Expression VisitMember(MemberExpression node)
+            {
+                if ((node.Member.MemberType & System.Reflection.MemberTypes.Property) != 0)
+                {
+                    MemberExpression newExpression = Expression.Property(Visit(node.Expression), node.Member.Name);
+                    return newExpression;
+                }
+                else if ((node.Member.MemberType & System.Reflection.MemberTypes.Field) != 0)
+                {
+                    MemberExpression newExpression = Expression.Field(Visit(node.Expression), node.Member.Name);
+                    return newExpression;
+                }
+                else
+                {
+                    return base.VisitMember(node);
+                }
+            }
+        }
+    }
+}
